b26db59bd2f289ca2b9b8ca162c9406eeea709d9
mauritiusmauritius46-alt <mauritiusmauritius46@gmail.com>
2026-09-13 00:52:22 -0300

    Use environment variable for GEMINI_API_KEY
    Updated GEMINI_API_KEY to use environment variable.

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, fil
 import google.generativeai as genai
 
 # Configuración de API Key de Gemini
-GEMINI_API_KEY = "AQ.Ab8RN6J3kmSO-PAWY4cVuFj6WeI6I9rTcS-5gJKOvUxn3XLCcg"
+GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
 genai.configure(api_key=GEMINI_API_KEY)
 
 # Personalidad de Mica
